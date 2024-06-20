887da5e9be8ef32339d2aa94566044100d10303a
Sam Briggs <git@sambriggs.dev>
2024-06-20 01:40:05 +0100

    Fix indexing embeddings
    Note: it's possible to have vespa compute embeddings internally. That
    might be something worth trying out next.

--- embeddings/README.md
+++ embeddings/README.md
@@ -1,3 +1,23 @@
 # Embeddings
 
 Pipline for generating embeddings for documents.
+
+## Coercing
+
+Flatten the data into a single object:
+
+```
+jq -c "{id: ._id} + ._source" ../filmfinder.vespa.app/ext/tmdb-dump.json > ../filmfinder.vespa.app/ext/tmdb-dump-flattened.jsonl
+```
+
+Then add the embeddings
+
+```
+poetry run embeddings ../filmfinder.vespa.app/ext/2-examples-flattened.jsonl ../filmfinder.vespa.app/ext/2-examples-flat-w-embeddings.jsonl
+```
+
+Then coerce into a format for vespa:
+
+```
+jq -c "{put:(\"id:film:film::\" + (.id | tostring)) , fields: {id, title, overview, tagline, release_date, genres, cast, directors}}" tmdb-dump-flat-w-embeddings.jsonl > tmdb-dump-vespa.jsonl
+```
--- embeddings/embeddings/embeddings.py
+++ embeddings/embeddings/embeddings.py
@@ -1,21 +1,9 @@
 from sentence_transformers import SentenceTransformer
 import click
 import pandas as pd
-from tqdm import tqdm
-from tqdm.contrib.concurrent import thread_map
 
 # Create the model
 model = SentenceTransformer("all-MiniLM-L6-v2")
-
-# Dummy function for now.
-def calculate_embedding(text):
-    # Add your actual implementation here
-    return text
-
-
-def process_record(record):
-    record['overview'] = calculate_embedding(record['overview'])
-    return record
 
 
 @click.command()
@@ -24,11 +12,9 @@ def process_record(record):
 def main(input_file, output_file):
   # Read JSON line file
   df = pd.read_json(input_file, lines=True)
-  # Take out
-  breakpoint()
   sentences = df["overview"].values
   embeddings = model.encode(sentences)
-  df["overview_embedding"] = embeddings
+  df["overview_embedding"] = embeddings.tolist()
   # Write to new JSON line file
   df.to_json(output_file, orient='records', lines=True)
 
